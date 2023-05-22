5cc4a7cf5af82f7917da2e8cc1485c16370d76ed
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-05-22 18:47:23 +0200

    eslint installed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
+/* eslint-disable no-unused-expressions */
+// eslint-disable-next-line import/extensions
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+'use strict';
+
+const galleryNew = document.querySelector('gallery');
+const addImg = galleryItems.map((image) => `<li class ="image"> <img src="${image.preview}" alt:"${image.description}"/></li>`)
+// eslint-disable-next-line indent, quotes
+    .join("");
+// eslint-disable-next-line no-console
+console.log(addImg);
+galleryNew.insertAdjacentHTML('afterbegin', addImg);
+// galleryNew.style.display = "flex";
